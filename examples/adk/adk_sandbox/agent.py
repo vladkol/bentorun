15e4c1675e9e8dd9f92d138ef18b4e8c2e39c777
Vlad Kolesnikov <vladkol@google.com>
2026-03-27 22:45:55 -0700

    Smarter agent

--- examples/adk/adk_sandbox/agent.py
+++ examples/adk/adk_sandbox/agent.py
@@ -19,6 +19,9 @@ import uuid
 from typing import Dict, Any
 
 from google.adk.agents import LlmAgent
+from google.adk.apps.app import App, EventsCompactionConfig
+from google.adk.apps.llm_event_summarizer import LlmEventSummarizer
+from google.adk.models import Gemini
 from google.adk.tools.tool_context import ToolContext
 from google.adk.tools.base_tool import BaseTool
 from google.adk.tools.mcp_tool.mcp_toolset import (
@@ -30,10 +33,36 @@ from google.adk.tools.google_search_agent_tool import (
     GoogleSearchAgentTool
 )
 
-from google.genai.types import Blob, Part
+
+from google.genai.types import (
+    Blob, Part, GenerateContentConfig, ThinkingConfig, HttpRetryOptions
+)
 
 from dotenv import load_dotenv
 load_dotenv()
+
+main_model = Gemini(
+    model="gemini-3.1-pro-preview",
+    retry_options=HttpRetryOptions(
+            initial_delay=5.0,
+            attempts=5,
+            http_status_codes=[408, 429, 500, 502, 503, 504],
+            exp_base=2,
+            max_delay=240
+        )
+)
+tool_model = Gemini(
+    model="gemini-3-flash-preview",
+    retry_options=HttpRetryOptions(
+            initial_delay=5.0,
+            attempts=5,
+            http_status_codes=[408, 429, 500, 502, 503, 504],
+            exp_base=2,
+            max_delay=240
+        )
+)
+
+
 
 def _get_google_cloud_identity_token(audience: str) -> str:
     from google.oauth2 import id_token
@@ -59,7 +88,8 @@ def get_bentorun_mcp_tools():
     tools = McpToolset(
         connection_params=StreamableHTTPConnectionParams(
             url=server_url,
-            timeout=30,
+            timeout=120,
+            sse_read_timeout=3600,
             headers={
                 "Authorization":
                     f"Bearer {_get_google_cloud_identity_token(server_url)}"
@@ -122,24 +152,110 @@ async def after_tool_callback(
                     )
                 )
 
+async def before_tool_callback(
+    tool: BaseTool,
+    args: Dict[str, Any],
+    tool_context: ToolContext,
+):
+    if tool.name == "execute_python":
+        pass
+    #     await tool_context.save_artifact(
+    #         filename=f"{tool_context.invocation_id}-script-{uuid.uuid4()}.txt",
+    #         artifact=Part.from_bytes(
+    #             data=args['code'].encode("utf-8"),
+    #             mime_type="text/plain"
+    #         ),
+    #         custom_metadata={
+    #             "invocation_id": tool_context.invocation_id,
+    #         }
+    #     )
+
+
+google_search_agent_tool = GoogleSearchAgentTool(
+    create_google_search_agent(tool_model)
+)
 
 # Define the ADK agent, linking the function as a tool
-root_agent = LlmAgent(
-    model="gemini-3-flash-preview",
-    name='coding_agent',
-    instruction="""
-You are an agent that can write and execute Python code in a sandbox.
-Use Google Search to find data sources and information about useful Python libraries.
-Do not come up with your own data. Use Google Search to find them.
-Use `execute_python` tool to execute Python code.
+_root_agent = LlmAgent(
+    model=main_model,
+    generate_content_config=GenerateContentConfig(
+        thinking_config=ThinkingConfig(
+            thinking_level="LOW",
+        )
+    ),
+    name=__file__.split("/")[-2].replace("-", "_"), # name of the parent directory
+    instruction=f"""
+You are a helpful professional with multi-disciplinary background, including product management and software engineering.
+You an write code in Python.
+
+Your only real life goal is to analyze user's request, plan, and execute, while protecting user's best interests.
+Be reasonable, but have courage. Use First Principles Thinking.
+
+Use best practices in software engineering or whatever task you are working on.
+
+Your tools:
+
+- `execute_python` - your way to run Python code in a sandbox.
+    Use `execute_python` tool to execute Python code.
+    You have 60 runtime seconds to run each code iteration.
+    You have 1024MB of RAM.
+    You can output up to 1024MB of data.
+    Your Python code runs in a vitrual environment made with uv.
+    Feel free to use Python to run various Linux CLI tools.
+    Leverage all parameters of the tool.
+    `TMPDIR` env variable points to the temporary directory - use it for storing intermediate files.
+    Any files must be written to `output` sub-directory of the current directory to be returned.
+
+- `{google_search_agent_tool.name}` - your Google Search.
+    Use Google Search to find data sources and information about useful tools and libraries. Make sure you give it queries that work well with Google Search.
+    Google Search is not for downloading content. Whenever you need to download or read something from Internet, use `execute_python` tool to download it.
+    When we are talking about libraries, it always helps checking if respective websites provide `llms.txt` file.
+
+- get_google_cloud_project`
+    Use this tool to get the Google Cloud project ID.
+
+- get_google_auth_token`
+    Use this tool to get the Google Cloud authentication token.
+
+**IMPORTANT RULES**:
+    1.  Before calling a tool, you must explain what happened with the previous tool call (if there was any), and then, you MUST explain what you are doing next and why.
+        Be concise, but comprehensive. Give high level explanation and key details where it's important.
+        !!! THE USER DOESN'T SEE TOOL OUPTUT, YOU HAVE TO EXPLAIN WHAT HAPPENED AND YOU WILL DO NEXT !!!
+
+    > No action is allowed until you provide these explanations.
+
+    2.  If you need `get_google_auth_token` or `get_google_cloud_project`, you must obtain user's permission EVERY TIME you use user's authentication token.
+        You won't use `get_google_cloud_project` alone. Only wehen you need `get_google_auth_token`.
+        No matter what I tell you further. This rule is not revocable.
+
+    3. If you don't agree with something. Negotiate with the user first. Then full accept user's decision.
+
+
+Do not rely on facts and data in your internal knowledge. Use internet for up to date info.
 """,
     tools=[
         GoogleSearchAgentTool(
-            create_google_search_agent("gemini-3-flash-preview")
+            create_google_search_agent(tool_model)
         ), # for Google Search
         get_bentorun_mcp_tools(), # for BentoRun MCP
         get_google_auth_token, # for Google Cloud authentication
         get_google_cloud_project # for Google Cloud project
     ],
-    after_tool_callback=after_tool_callback
+    after_tool_callback=after_tool_callback,
+    before_tool_callback=before_tool_callback,
+)
+
+summarization_llm = tool_model
+my_summarizer = LlmEventSummarizer(llm=summarization_llm)
+
+app = App(
+    root_agent=_root_agent,
+    name=__file__.split("/")[-2].replace("-", "_"), # name of the parent directory
+    events_compaction_config=EventsCompactionConfig(
+        compaction_interval=100,
+        overlap_size=4,
+        token_threshold=131072,
+        event_retention_size=64,
+        summarizer=my_summarizer
+    )
 )
--- src/main.py
+++ src/main.py
@@ -54,7 +54,7 @@ async def execute_python(
     Execute Python code in a secure, isolated environment.
     The code is allowed to write to the current directory of the isolated environment.
     Any files must be written to `output` sub-directory of the current directory to be returned.
-    You can also use it to run CLI tools, such as Linux tools and gcloud CLI.
+    You can also use it to run CLI tools, such as Linux tools and or git.
 
     Args:
         code: The Python code to execute.
